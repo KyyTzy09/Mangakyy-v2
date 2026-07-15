c327025a71961dbef48b5dd1af1e3addec570f0d
Firegame224 <xg474627@gmail.com>
2026-07-16 00:27:57 +0700

    feat(comic): add ChapterCard widget for displaying chapter details

---
+++ lib/navigations/widget/chapter/chapter_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:mangakyy_v2_mobile/core/colors/app_color.dart';
+
+class ChapterCard extends StatelessWidget {
+  const ChapterCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Card(
+      color: AppColor.card,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: EdgeInsets.all(screenWidth * 0.02),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  'https://i.pinimg.com/736x/0d/8c/5b/0d8c5b8118f427939a12560cca76158e.jpg',
+                  fit: BoxFit.cover,
+                  height: 100,
+                  width: 200,
+                  errorBuilder: (context, error, stackTrace) => Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.grey,
+                    child: Icon(Icons.error),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  spacing: 4,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Chapter 1',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "5 days ago",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: screenWidth * 0.035,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle button press
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColor.primary,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                'Baca',
+                style: TextStyle(fontSize: screenWidth * 0.035),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/navigations/widget/comic/comic_widget.dart
+++ lib/navigations/widget/comic/comic_widget.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mangakyy_v2_mobile/common/models/comic_model.dart';
 import 'package:mangakyy_v2_mobile/core/colors/app_color.dart';
+import 'package:mangakyy_v2_mobile/navigations/widget/chapter/chapter_card.dart';
 import 'package:mangakyy_v2_mobile/navigations/widget/comic/comic_content.dart';
 import 'package:mangakyy_v2_mobile/navigations/widget/comic/comic_header.dart';
 
@@ -19,35 +20,29 @@ class ComicWidget extends StatelessWidget {
           return CustomScrollView(
             slivers: [
               ComicHeader(comic: comic),
-              SliverFillRemaining(hasScrollBody: false, child: ComicContent(),),
+              SliverFillRemaining(hasScrollBody: false, child: ComicContent()),
+              SliverToBoxAdapter(
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: AppColor.primary, width: 1),
+                    ),
+                  ),
+                  padding: EdgeInsets.only(bottom: 10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Text(
+                    "Chapter List",
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.045,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
               SliverList(
                 delegate: SliverChildBuilderDelegate((context, index) {
-                  return Container(
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: AppColor.card,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Chapter " + (index + 1).toString(),
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          comic.description ?? "",
-                          style: TextStyle(color: Colors.white70, fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  );
+                  return ChapterCard();
                 }, childCount: 20),
               ),
             ],
